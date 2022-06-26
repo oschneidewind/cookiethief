aa79fe6041b798d30af3ec2c7b707a809b1a3ba6
Oliver Schneidewind <schneidewind@printf-debugging.de>
2022-06-26 12:06:52 +0200

    SqliteCookieJar remove knowledge about database
    Until now, the (base) class SqliteCookieJar made assumptions about the
    database structure of the SQLite database. These assumptions may be
    correct for Firefox, but if other SQLite databases are also processed,
    they are certainly not correct.
    
    Now the complete knowledge about the database is in the method
    _cookiefromsql which can be implemented individually by each class.

--- cookiethief.py
+++ cookiethief.py
@@ -6,6 +6,7 @@ import contextlib
 import os
 import sys
 import tempfile
+import typing
 import sqlite3
 import http.cookiejar
 
@@ -28,54 +29,54 @@ class SqliteCookieJar(http.cookiejar.FileCookieJar):
                 raise ValueError(http.cookiejar.MISSING_FILENAME_TEXT)
 
         with contextlib.ExitStack() as stack:
-            sql = stack.enter_context(tempfile.NamedTemporaryFile(suffix='.sqlite'))
-            fin = stack.enter_context(open(filename, 'rb'))
+            sql = stack.enter_context(
+                tempfile.NamedTemporaryFile(suffix=".sqlite"))
+            fin = stack.enter_context(open(filename, "rb"))
 
             # Copy sqlite to tmpfile for avoid sqlite lock issues
-            for chunk in iter(fin.read, b''):
-                sql.write(chunk)
+            [sql.write(x) for x in iter(fin.read, b'')]
 
             sqlite = stack.enter_context(sqlite3.connect(sql.name))
             # get a result as dict
             sqlite.row_factory = sqlite3.Row
 
-            for cdict in self._getfromsql(sqlite):
-                try:
-                    startisdot = cdict['host'].startswith('.')
-                    cookie = http.cookiejar.Cookie(0,
-                                                   cdict['name'],
-                                                   cdict['value'],
-                                                   None, False,
-                                                   cdict['host'],
-                                                   startisdot,
-                                                   startisdot,
-                                                   cdict['path'], False,
-                                                   cdict['isSecure'],
-                                                   cdict['expiry'],
-                                                   cdict['expiry'],
-                                                   None, None, {})
-                    if (ignore_expires and cookie.is_expired) or \
-                       (ignore_discard and cookie.discard):
+            for cookie in self._cookiefromsql(sqlite):
+                if (ignore_expires and cookie.is_expired) or \
+                   (ignore_discard and cookie.discard):
                        continue
-                    self.set_cookie(cookie)
-                except KeyError:
-                    msg = "Coudn't convert db entry {} to cookie".format(cdict)
-                    raise SqliteCookieError(msg)
+                self.set_cookie(cookie)
 
-    def _getfromsql(self, sqlite):
+    def _cookiefromsql(self, sqlite: sqlite3.Connection) -> typing.Iterator[http.cookiejar.Cookie]:
         raise NotImplementedError
 
 
 class FirefoxCookieJar(SqliteCookieJar):
 
-    def _getfromsql(self, sqlite):
+    def _cookiefromsql(self, sqlite):
         keys = ['host', 'path', 'isSecure', 'expiry', 'name', 'value']
         query = 'select {keys} from moz_cookies'.format(keys=','.join(keys))
 
         cur = sqlite.cursor()
         cur.execute(query)
         for item in cur.fetchall():
-            yield item
+            try:
+                startisdot = item['host'].startswith('.')
+                cookie = http.cookiejar.Cookie(0,
+                                               item['name'],
+                                               item['value'],
+                                               None, False,
+                                               item['host'],
+                                               startisdot,
+                                               startisdot,
+                                               item['path'], False,
+                                               item['isSecure'],
+                                               item['expiry'],
+                                               item['expiry'],
+                                               None, None, {})
+                yield cookie
+            except KeyError as exp:
+                msg = f"Coudn't convert db entry {item} to cookie"
+                raise SqliteCookieError(msg) from exp
 
     @staticmethod
     def find_profile():
